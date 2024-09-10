8de654c05ccb909469e9889cdc8b0107b0435794
Wilson Soetomo <wilsonsoetomo@Wilsons-MacBook-Pro.local>
2024-09-10 11:15:28 -0700

    Fixed login authentication to get playlists

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import requests
 from urllib.parse import urlencode
-import datetime
+from datetime import datetime
 
 load_dotenv()
 
@@ -23,7 +23,7 @@ SPOTIFY_SCOPES    = "user-top-read"
 def index():
     return "Welcome! Let's see how weird your music is <a href='/login'>Login with Spotify</a>"
 
-@app.route('login')
+@app.route('/login')
 def login():
     scope = 'user-read-private user-read-email'
 
@@ -43,7 +43,7 @@ def callback():
         return jsonify({'error': request.args['error']})
     
     if 'code' in request.args:
-        req_body ={
+        req_body = {
             'code': request.args['code'],
             'grant_type': 'authorization_code',
             'redirect_uri': SPOTIFY_REDIRECT_URI,
@@ -54,11 +54,10 @@ def callback():
         response = requests.post(SPOTIFY_TOKEN_URL, data = req_body)
         token_info = response.json()
 
-        session['access_token '] = token_info['access_token ']
-        session['refresh_token '] = token_info['refresh_token ']
-        session['expires_at'] = datetime.now().timestamp() +token_info['expires_in']
-
+        session['access_token'] = token_info['access_token']
+        session['expires_at'] = datetime.now().timestamp() + 10
         return redirect('/playlists')
+
     
 @app.route('/playlists')
 def get_playlists():
@@ -66,13 +65,14 @@ def get_playlists():
         return redirect('/login')
     
     if datetime.now().timestamp() > session['expires_at']:
+        print('TOKEN EXPIRED, REFRESHING....')
         return redirect('/refresh-token')
     
     headers = {
-        'Authorization' : f"Bearer ${session['access_token']}"
+        'Authorization' : f"Bearer {session['access_token']}"
     }
 
-    response = requests.get(SPOTIFY_API_URL + 'me/playlists', headers = headers)
+    response = requests.get(SPOTIFY_API_URL + '/me/playlists', headers = headers)
     playlists = response.json()
 
     return jsonify(playlists)
@@ -93,3 +93,9 @@ def refresh_token():
         new_token_info = response.json()
 
         session['access_token'] = new_token_info['access_token']
+        session['expires_at'] =  datetime.now().timestamp() + new_token_info['expires_in']
+
+        return redirect('/playlists')
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)

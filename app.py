cdbe2061f0b9d3496b1a50d9a7f184c7d4730f46
Wilson Soetomo <wilsonsoetomo@Wilsons-MBP.attlocal.net>
2024-09-09 21:37:47 -0700

    Fixing refresh token

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import requests
 from urllib.parse import urlencode
+import datetime
 
 load_dotenv()
 
@@ -42,7 +43,7 @@ def callback():
         return jsonify({'error': request.args['error']})
     
     if 'code' in request.args:
-        req_budy ={
+        req_body ={
             'code': request.args['code'],
             'grant_type': 'authorization_code',
             'redirect_uri': SPOTIFY_REDIRECT_URI,
@@ -50,4 +51,45 @@ def callback():
             'client_secret': SPOTIFY_CLIENT_SECRET
         }
 
-        response = requests.post()
+        response = requests.post(SPOTIFY_TOKEN_URL, data = req_body)
+        token_info = response.json()
+
+        session['access_token '] = token_info['access_token ']
+        session['refresh_token '] = token_info['refresh_token ']
+        session['expires_at'] = datetime.now().timestamp() +token_info['expires_in']
+
+        return redirect('/playlists')
+    
+@app.route('/playlists')
+def get_playlists():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    
+    headers = {
+        'Authorization' : f"Bearer ${session['access_token']}"
+    }
+
+    response = requests.get(SPOTIFY_API_URL + 'me/playlists', headers = headers)
+    playlists = response.json()
+
+    return jsonify(playlists)
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type' : 'refresh_token',
+            'refresh_token': session['refresh_token'],
+            'client_id': SPOTIFY_CLIENT_ID,
+            'client_secret': SPOTIFY_CLIENT_SECRET
+        }
+        response = requests.post(SPOTIFY_TOKEN_URL, data = req_body)
+        new_token_info = response.json()
+
+        session['access_token'] = new_token_info['access_token']
